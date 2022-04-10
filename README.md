549d65104c43023e0978b19c22d422be0ab1179f
cesarazeredo <cesar.azeredo@uol.com.br>
2022-04-10 14:58:11 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-vz5wju
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-vz5wju)
---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { test_Qualisel } from '@angular/core';
+
+@Component({
+  selector:  'test_Qualisel',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+}
